a4f7325efffd0b3a30f1c456cb6338994396f2f5
yosoyducc <59681403+yosoyducc@users.noreply.github.com>
2024-12-03 17:49:37 -0800

    Add elementary UI

--- MedFlix.cpp
+++ MedFlix.cpp
@@ -58,6 +58,23 @@ MedFlix::MedFlix() : db("database.ini")
     background = GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR));
 }
 
+// Left bar variables (temp)
+static string const medFlix  = "MedFlix";
+static string const listText = "#185#Home;#186#Favorites;#043#Search;#151#Account;#159#Quit";
+static string const tempText = "Press the \"MedFlix\" button to \"log in\"!";
+static string const status   = "MedFlix 0.1.0  |  Copyright © 2024 Aiden Asar & Filip Vracevic. All Rights Reserved."; 
+
+static int listScrollIdx = 0;
+static int listActive = 0;
+static int loggedIn = 0;
+
+static Rectangle layouts[] = {
+    { 8, 8, 144, 40 },      // button rectangle
+    { 8, 48, 144, 272 },    // list rectangle
+    { 192, 16, 336, 72 },   // dummy placeholder text rectangle
+    { 0, 0, 0, 24 },        // statusbar rectangle
+};
+
 void MedFlix::update()
 {
     // Test if conditions are met to draw the exit box
@@ -65,6 +82,11 @@ void MedFlix::update()
         exitPrompt = !exitPrompt;
     if (WindowShouldClose())
         programShouldClose = true;
+
+    // subtract padding, starting y position, and status bar
+    layouts[1].height = GetScreenHeight() - 8 - 48 - layouts[3].height;
+    layouts[3].y = GetScreenHeight() - layouts[3].height;
+    layouts[3].width = GetScreenWidth();
 }
 
 void MedFlix::render()
@@ -73,8 +95,24 @@ void MedFlix::render()
     BeginDrawing();
         ClearBackground(background);
 
+        // Draw left hand menu bar
+        if (GuiButton(layouts[0], medFlix.data())) {
+            // temporary fake login when pressing the MedFlix button
+            loggedIn = !loggedIn;
+            listActive = 0;
+        }
+        // offset the char array to only display account page for sign in
+        GuiListView(layouts[1], loggedIn ? listText.data() : listText.data() + 37, &listScrollIdx, &listActive);
+
+        // Draw placeholder rectangle if not "signed in"
+        if (!loggedIn)
+            GuiDummyRec(layouts[2], tempText.data());
+
+        // Draw status bar at bottom of program
+        GuiStatusBar(layouts[3], status.data());
+
         // Turn on word wrap and draw the text box with sample description
-        GuiSetStyle(DEFAULT, TEXT_ALIGNMENT_VERTICAL, TEXT_ALIGN_TOP);
+        /*GuiSetStyle(DEFAULT, TEXT_ALIGNMENT_VERTICAL, TEXT_ALIGN_TOP);
         GuiSetStyle(DEFAULT, TEXT_WRAP_MODE, TEXT_WRAP_WORD);
         // yes it's temporary but still horrible
         // we can copy this string only once and not every game loop lol
@@ -83,7 +121,7 @@ void MedFlix::render()
         // Draw the text starting after the opening quote
         GuiTextBox((Rectangle){ 40, 40, 300, 175 }, (char *)dat.data() + 1, 0, false);
         GuiSetStyle(DEFAULT, TEXT_WRAP_MODE, TEXT_WRAP_NONE);
-        GuiSetStyle(DEFAULT, TEXT_ALIGNMENT_VERTICAL, TEXT_ALIGN_MIDDLE);
+        GuiSetStyle(DEFAULT, TEXT_ALIGNMENT_VERTICAL, TEXT_ALIGN_MIDDLE);*/
 
         // Draw the exit box if required
         if (exitPrompt) {
