b8d0f7400da3f022a3080b744aba737f9e6c1a5e
AidenAsar <titlewave6@gmail.com>
2024-12-09 09:01:55 +1200

    Update HashTable.h
    matches cpp file now

--- HashTable.h
+++ HashTable.h
@@ -18,10 +18,13 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "AccountManager.h"
+#include <random>
+#include <algorithm>
 
 //                                      (class)      (rating)      (students)
 //                                    v.first.first  .second        v.second
-typedef std::vector<std::pair<std::pair<std::string, float>, std::vector<int>>> prof_data;
+//typedef std::vector<std::pair<std::pair<std::string, float>, std::vector<int>>> prof_data;
 //what the teacher's name will be linked to
 
 struct MovieNode {
@@ -47,46 +50,46 @@ class HashTable {
 private:
     static const int SIZE = 1453;
     MovieNode* dataMap[SIZE];
-    //std::vector<MovieNode*> fav;
-    //std::vector<int> watched;
-    //std::vector<MovieNode*> liked;
     int hash(std::string const &key);
-
 public:
+    std::vector<MovieNode*> Action;
+    std::vector<MovieNode*> Comedy;
+    std::vector<MovieNode*> Drama;
+    std::vector<MovieNode*> Scifi;
+    std::vector<MovieNode*> Horror;
     ~HashTable();
     //adds a teacher and their information to the linked list
-    void set(std::string hashname, const std::string &name1,const std::string &year1,const std::string &rating1,const std::string &runtime1,
+    MovieNode* set(std::string hashname, const std::string &name1,const std::string &year1,const std::string &rating1,const std::string &runtime1,
 const std::string &genre1,const std::string &director1,const std::string &descript1,const std::string &imdb1,const std::string &poster1);
 
-    // For a given professor name, get courses
+    // For a giveMovieNode*n professor name, get courses
     //prof_data const *getCourses(std::string &name);
 
     //creates a list of all teachers
-    std::vector<std::string> getNames();
+    //std::vector<std::string> getNames();
 
     //adds a student to a course
-    bool addStudent(int id, std::string const &teacher, std::string const &course);
+    //bool addStudent(int id, std::string const &teacher, std::string const &course);
 
     //adds student at the start of the program
-    void addStudentText(int id, std::string const &prof, std::string const &course);
+    //void addStudentText(int id, std::string const &prof, std::string const &course);
 
     //adds a rating for a teacher
-    void changeRating(std::string course, std::string teacher, float newRating, int id);
+    //void changeRating(std::string course, std::string teacher, float newRating, int id);
 
     //updates the teacher txt file when the program ends
-    void updateTable();
+    //void updateTable();
 
     //returns a list of all teachers who teach a specific course
-    std::vector<std::pair<std::string, float>> findClass(std::string &course);
+    //std::vector<std::pair<std::string, float>> findClass(std::string &course);
 
     // returns a list of all courses student is registered in
-    std::vector<std::pair<std::string, std::string>> getRegistered(int ID);
+    //std::vector<std::pair<std::string, std::string>> getRegistered(int ID);
 
     // check if id already registered
-    bool check(int id, std::string &teacher, std::string &course);
+    //bool check(int id, std::string &teacher, std::string &course);
+    std::vector<MovieNode*> search(std::string movieSearch);
 
-    std::vector<MovieNode*> search(std::string &movieSearch);
-
-    void recommend();
+    std::vector<MovieNode*> recommend(AccountManager& accountReader, IniReader const &db);
 };
 
